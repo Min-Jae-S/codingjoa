164930871ac2330d8882e439c23794651a2f115b
User <User@DESKTOP-FPDF7SA>
2022-10-14 14:09:04 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/config/EmailConfig.java
+++ codingjoa/src/main/java/com/codingjoa/config/EmailConfig.java
@@ -2,7 +2,7 @@ package com.codingjoa.config;
 
 import java.util.Properties;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -14,6 +14,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 @PropertySource("/WEB-INF/properties/mail.properties")
 public class EmailConfig {
 	
+	@Autowired
 	private Environment env;
 
 	@Bean
