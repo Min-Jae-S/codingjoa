8cd8e26481f27bdeae197e7dd21d400e097e9a51
User <User@DESKTOP-FPDF7SA>
2022-10-20 11:27:26 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/security/config/SecurityConfig.java
+++ codingjoa/src/main/java/com/codingjoa/security/config/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @ComponentScan("com.codingjoa.security.service")
@@ -42,14 +44,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		//super.configure(http);
 		
-		/*
 		CharacterEncodingFilter filter = new CharacterEncodingFilter();
 		filter.setEncoding("UTF-8");
 		filter.setForceEncoding(true);
-		http.addFilterBefore(filter, CsrfFilter.class);
-		*/
-
-		http
+		
+		http.addFilterBefore(filter, CsrfFilter.class)
 			.csrf().disable()
 			.authorizeRequests()
 				.anyRequest().permitAll()
