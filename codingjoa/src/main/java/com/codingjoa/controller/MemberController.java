98309a0f71c121952c2c244272106c981d02f789
User <User@DESKTOP-FPDF7SA>
2022-11-01 16:02:07 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/controller/MemberController.java
+++ codingjoa/src/main/java/com/codingjoa/controller/MemberController.java
@@ -12,17 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.codingjoa.dto.EmailDto;
-import com.codingjoa.dto.EmailResponseDto;
 import com.codingjoa.dto.JoinDto;
 import com.codingjoa.dto.LoginDto;
-import com.codingjoa.service.EmailService;
 import com.codingjoa.service.MemberService;
-import com.codingjoa.util.MessageUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -34,24 +28,12 @@ public class MemberController {
 	@Autowired
 	private MemberService memberService;
 	
-	@Autowired
-	private EmailService emailService;
-	
 	@Resource(name = "joinValidator")
 	private Validator joinValidator;
 	
-	@Resource(name = "emailValidator")
-	private Validator emailValidator;
-	
-	@InitBinder
+	@InitBinder("joinDto")
 	public void initBinder(WebDataBinder binder) {
-		String objectName = binder.getObjectName();
-
-		if(objectName.equals("joinDto")) {
-			binder.addValidators(joinValidator);
-		} else if(objectName.equals("emailDto")) {
-			binder.addValidators(emailValidator);
-		}
+		binder.addValidators(joinValidator);
 	}
 	
 	@GetMapping("/join")
@@ -68,29 +50,9 @@ public class MemberController {
 		if(bindingResult.hasErrors()) {
 			return "member/join";
 		}
-		
 		memberService.register(joinDto);
 		
 		return "member/join-success"; 
-	}
-	
-	@PostMapping("/sendAuthEmail")
-	@ResponseBody
-	public EmailResponseDto sendAuthEmail(@Valid @RequestBody EmailDto emailDto, BindingResult bindingResult) {
-		log.info("sendAuthEmail, {}", emailDto);
-		
-		EmailResponseDto emailResponseDto = new EmailResponseDto();
-		
-		if(bindingResult.hasErrors()) {
-			String code = bindingResult.getAllErrors().get(0).getCodes()[0];
-			emailResponseDto.setErrorMessage(MessageUtils.getMessage(code));
-			emailResponseDto.setValidated(false);
-		} else {
-			emailResponseDto.setValidated(true);
-			emailService.sendAuthEmail(emailDto);
-		}
-		
-		return emailResponseDto;
 	}
 	
 	@RequestMapping("/login")
