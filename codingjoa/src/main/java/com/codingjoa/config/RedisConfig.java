5f6e09eda3933ce5c222a2146fea53d1181d522e
User <User@DESKTOP-FPDF7SA>
2022-11-08 20:57:18 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/config/RedisConfig.java
+++ codingjoa/src/main/java/com/codingjoa/config/RedisConfig.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @PropertySource("/WEB-INF/properties/redis.properties")
